e80a46549f46ed906aa8eefb879fcae53cdd8a16
khawam9 <khawam9@mcmaster.ca>
2026-09-08 17:06:00 -0400

    add math code
    added some math code

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -1,7 +1,13 @@
 #include <iostream>
 using namespace std;
 
+int a = 5;
+int b = 11;
+int c;
+
 int main(){
     cout <<"Hello World";
+    c = a + b;
+    std::cout << c << std::endl;
     return 0;
 }
